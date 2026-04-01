bca5d029a04a696fc796ec13a9c8255935b40b97
Giorgio Amati <g.amati@cineca.it>
2026-04-02 00:46:15 +0200

    Add files via upload

--- NVML_example/compile.sleep.sh
+++ NVML_example/compile.sleep.sh
@@ -1,18 +1,23 @@
 #
-rm -rf *.o mm.x *.mod
+rm -rf *.o sleep.x *.mod
 #
 # nvfortran (NVIDIA)
-COMP=nvfortran
-OPT="-O3 -cpp -DNVML -Minfo=accel"
+FC=nvfortran
+CC=nvcc
+OPT="-O3 -Minfo=all"
+#
+INC1=/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/include
+LIB1="/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/lib/stubs -lnvidia-ml"
 #
 echo "compiling GPU with " $COMP $OPT 
 #
-nvcc -O3 -I/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/include -DNVML -c nvml_wrapper.c
-$COMP $OPT -c nvml_interface_module.f90
-$COMP $OPT mod_tools.F90 -c
-$COMP $OPT sleep.F90 -c
+$CC -O3 -I$INC1 -DNVML -c nvml_wrapper.c
 #
-$COMP $OPT mod_tools.o nvml_interface_module.o nvml_wrapper.o sleep.o -o sleep.x -L/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/lib/stubs -lnvidia-ml
+$FC $OPT -DNVML -c nvml_interface_module.F90
+$FC $OPT -c mod_tools.F90
+$FC $OPT -c sleep.F90 
+#
+$FC $OPT mod_tools.o nvml_interface_module.o nvml_wrapper.o sleep.o -o sleep.x -L$LIB1
 #
 echo "That's all folks!!!"
 #
