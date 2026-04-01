44db49d770f871c91a41eb7013e1775e9c7e9ec6
Giorgio Amati <g.amati@cineca.it>
2026-04-01 20:47:32 +0200

    Add files via upload

---
+++ NVML_example/nvml_wrapper.c
@@ -0,0 +1,34 @@
+#ifdef NVML
+#include <nvml.h>
+  
+int get_gpu_power_index(int idx) {
+    static int inited = 0;
+    if(!inited){
+        if (nvmlInit_v2() != NVML_SUCCESS) return -1;
+        inited = 1;
+    }
+
+    nvmlDevice_t dev;
+    if (nvmlDeviceGetHandleByIndex_v2(idx, &dev) != NVML_SUCCESS) return -2;
+
+    unsigned int p_mw = 0;
+    if (nvmlDeviceGetPowerUsage(dev, &p_mw) != NVML_SUCCESS) return -3;
+
+    return (int)p_mw;
+}
+
+int get_gpu_energy_mJ_u64(int idx, unsigned long long *e_mJ){
+  static int inited = 0;
+  if(!inited){
+    if(nvmlInit_v2() != NVML_SUCCESS) return -1;
+    inited = 1;
+  }
+  nvmlDevice_t dev;
+  if(nvmlDeviceGetHandleByIndex_v2(idx, &dev) != NVML_SUCCESS) return -2;
+
+  nvmlReturn_t r = nvmlDeviceGetTotalEnergyConsumption(dev, e_mJ);
+  if(r != NVML_SUCCESS) return -3;
+  return 0;
+}
+
+#endif
