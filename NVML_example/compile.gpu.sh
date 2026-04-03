87f13c4be07479f22c5cc559f8c1013acd738ad4
Giorgio Amati <g.amati@cineca.it>
2026-04-03 17:10:12 +0200

    Add files via upload
    Added path for pitagora

--- NVML_example/compile.gpu.sh
+++ NVML_example/compile.gpu.sh
@@ -5,8 +5,14 @@ rm -rf *.o mm.x *.mod
 FC=nvfortran
 CC=nvcc
 OPT="-O3 -DNVML -cpp -acc -gpu=cc80 -Minfo=accel"
-INC1=/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/include
-LIB1="/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/lib/stubs -lnvidia-ml"
+#
+# Pitagora
+INC1=/pitagora/prod/spack/6.1/install/0.22/linux-rhel9-zen4/gcc-11.4.1/nvhpc-25.11-ntshdsgl52b6ckb6iu7xazd6uvqi3wqi/Linux_x86_64/25.11/cuda/13.0/targets/x86_64-linux/include
+LIB1="/pitagora/prod/spack/6.1/install/0.22/linux-rhel9-zen4/gcc-11.4.1/nvhpc-25.11-ntshdsgl52b6ckb6iu7xazd6uvqi3wqi/Linux_x86_64/25.11/cuda/13.0/targets/x86_64-linux/lib/stubs -lnvidia-ml"
+#
+# Leonardo
+##INC1=/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/include
+#LIB1="/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.3-v63z4inohb4ywjeggzhlhiuvuoejr2le/Linux_x86_64/24.3/cuda/12.3/targets/x86_64-linux/lib/stubs -lnvidia-ml"
 #
 echo "compiling GPU with " $COMP $OPT 
 #
