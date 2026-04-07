8618506a4b6ec449c041550fd14dbde4f0fee924
Giorgio Amati <g.amati@cineca.it>
2026-04-07 18:44:27 +0200

    Add files via upload

---
+++ CHECK_GPU/streamBlockSize.node.F90
@@ -0,0 +1,120 @@
+! ----------------------------------------------------
+! simple copy Memory-to-Memory kernel...
+!
+      module m
+              use, intrinsic :: iso_fortran_env
+              integer, parameter :: fpKind = real64
+      contains
+            attributes (global) subroutine copy (lhs, rhs, n)
+              implicit none
+              real(fpKind):: lhs(*)
+              real(fpKind):: rhs(*)
+              integer, value :: n
+              integer :: i
+              i = blockDim%x * (blockIdx%x-1) + threadIdx%x  
+              if (i<=n) lhs(i) = rhs(i)
+            end subroutine copy
+      end module m
+!
+! ----------------------------------------------------
+      program streamBlockSize
+!
+! simple stream code (borrowed) from M. Fatica book (2nd edition)
+! computer BW as a function of the Blocksize (max 1024)
+!
+          use cudafor
+          use m
+!
+          implicit none
+!              
+          integer, parameter   :: n=(2*64*1024*1024)
+          real(fpKind), device :: a_d(n)        !lhs
+          real(fpKind), device :: b_d(n)        !rhs
+          real(fpKind)         :: a(n)          !check
+          real :: time
+          real :: somma
+          real :: peakBW, sustBW
+          integer :: iter, i, igpu
+          integer :: istat 
+          integer :: nDevices
+          integer(8) :: nBytes
+          integer :: nBlocks, blockSize
+          type(cudaevent) :: startEvent, stopEvent
+          type(cudaDeviceProp) :: prop
+! running on 4 GPU
+          do igpu=0,3
+!
+! set blocksize et al...
+             blockSize = 8
+             nBlocks = (n-1)/blockSize+1
+             nBytes = 2*sizeof(a_d)
+!
+             istat = cudaGetDeviceProperties(prop,igpu)
+!
+! theoretical peak
+             peakBW = 2.0*(prop%memoryBusWidth/8)*  &
+                      (prop%memoryClockRate/1000)/  &
+                      1000
+!           
+             write(6,*) "-------------------------"
+             write(6,*) "# GPU            ", igpu
+             write(6,*) "GPU              ", trim(prop%name)
+             write(6,*) "GPU freq.   (Mhz)", prop%memoryClockRate/1000
+             write(6,*) "Bus width  (bits)", prop%memoryBusWidth
+             write(6,*) "Peak BW    (GB/s)", peakBW
+             write(6,*) "Vector Size  (MB)        ", n/1024/1024
+             write(6,*) "-------------------------"
+             write(6,*) "BlockSize, Meas.BW, Peak BW(GB/s) "
+!
+! loop over blocksize (8,16,32,64,128,256,512,1024)
+             do iter = 0, 7
+!
+! vector allocate & initialization          
+                a_d=-666*1.0_fpKind
+                b_d= (iter+1)*1.0_fpKind
+!             
+                istat = cudaEventCreate(startEvent)
+                istat = cudaEventCreate(stopEvent)
+!
+! copy section
+                istat = cudaDeviceSynchronize()
+                istat = cudaEventRecord(startEvent,0)
+!          
+                call copy <<< nBlocks, blockSize >>> (a_d,b_d,n)
+!          
+                istat = cudaEventRecord(stopEvent, 0)
+                istat = cudaEventSynchronize(stopEvent)
+                istat = cudaEventElapsedTime(time,startEvent,stopEvent)
+!          
+                istat = cudaEventDestroy(startEvent)
+                istat = cudaEventDestroy(stopEvent)
+!
+                sustBW = nBytes/time/1000000
+!          
+                write(6,*) blockSize, sustBW, peakBW
+!             write(6,*) "-------------------------"
+!             write(6,*) "    "
+!
+! check  (only last 100 elements)
+                somma = 0
+                do i = 1, n, 10000
+                   a(i) = a_d(i)
+                   somma  = somma - (a(i) - (iter+1)*1.0_fpKind)
+                enddo
+!            
+                if (somma == 0) then
+! do nothing
+                else
+                    write(6,*) "ERROR: somma is not zero ", somma
+                endif    
+!             
+! new blockSize (et al...)             
+                blockSize = 2*blockSize
+                nBlocks = (n-1)/blockSize+1
+!              
+             enddo
+          enddo
+!              
+      end program streamBlockSize
+!
+
